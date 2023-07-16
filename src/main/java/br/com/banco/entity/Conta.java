1e8f1a715fc233967b3299c247f90b6b217dca21
Roberta de Siqueira <designer.roberta@gmail.com>
2023-07-16 11:48:16 -0300

    entities e repositories criados

---
+++ src/main/java/br/com/banco/entity/Conta.java
@@ -0,0 +1,25 @@
+package br.com.banco.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="conta")
+public class Conta implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idConta;
+
+    @NotNull
+    private String nomeResponsavel;
+}
---
+++ src/main/java/br/com/banco/entity/Transferencia.java
@@ -0,0 +1,38 @@
+package br.com.banco.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="transferencia")
+public class Transferencia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDateTime dataTransferencia;
+
+    private BigDecimal valor;
+
+    private String tipo;
+
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+}
